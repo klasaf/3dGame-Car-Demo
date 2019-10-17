0a61095b8760fb1da723b108489c1c6c5af38b3c
Francesco Clasadonte <klasaf@outlook.com>
2019-10-17 16:46:17 +0200

    added project

---
+++ Assets/Scripts/CollisionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHelper : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "veichle")
+        {
+            var particleSystem = gameObject.GetComponent<ParticleSystem>();
+            if(particleSystem!=null && !particleSystem.isPlaying)
+            {
+                particleSystem.Play();
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                Destroy(gameObject, 2f);
+            }
+        }
+    }
+}
